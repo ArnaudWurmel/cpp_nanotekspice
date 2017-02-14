b133a56c335b272879041b20adc0b01b67f66be5
Victorien Fischer <victorien.fischer@epitech.eu>
2017-02-14 15:58:25 +0100

    Added some files to ignore in the gitignore file. Added IComponent.hpp in the interface folder.

---
+++ Interface/IComponent.hpp
@@ -0,0 +1,35 @@
+//
+// IComponent.hpp for NanoTekSpice in /home/vicky/Bureau/EPITECH/tek2-2/C++/cpp_nanotekspice
+// 
+// Made by Victorien Fischer
+// Login   <victorien.fischer@epitech.eu>
+// 
+// Started on  Tue Feb 14 15:50:27 2017 Victorien Fischer
+// Last update Tue Feb 14 15:55:43 2017 Victorien Fischer
+//
+
+#ifndef __ICOMPONENT_HPP__
+# define __ICOMPONENT_HPP__
+
+namespace	nts
+{
+  enum		Tristate
+    {
+      UNDEFINED = (-true),
+      TRUE = true,
+      FALSE = false
+    };
+
+  class		IComponent
+  {
+  public:
+    virtual nts::Tristate	Compute(size_t pin_num_this = 1) = 0;
+    virtual void		SetLink(size_t pin_num_this,
+					nts::IComponent &component,
+					size_t pin_num_target) = 0;
+    virtual void		Dump(void) const = 0;
+    virtual			~IComponent(void) { }
+  };
+}
+
+#endif // __ICOMPONENT_HPP__
