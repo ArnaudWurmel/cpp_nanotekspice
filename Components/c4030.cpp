be85c21057566e340adbe2004b27e2d0777dbabd
Arnaud WURMEL <arnaud.wurmel@epitech.eu>
2017-02-28 14:36:10 +0100

    Xor

--- Components/c4030.cpp
+++ Components/c4030.cpp
@@ -5,25 +5,90 @@
 // Login   <victorien.fischer@epitech.eu>
 // 
 // Started on  Tue Feb 14 16:40:02 2017 Victorien Fischer
-// Last update Wed Feb 15 20:54:15 2017 Arnaud WURMEL
+// Last update Tue Feb 28 14:29:15 2017 Arnaud WURMEL
 //
 
+#include <iostream>
+#include <string>
+#include "Errors.hpp"
 #include "c4030.hpp"
 
-nts::c4030::c4030(const std::string &value)
+nts::c4030::c4030(const std::string &value) : _links(0)
 {
   (void)value;
 }
 
+bool	nts::c4030::getValueForPin(size_t pin)
+{
+  std::vector<std::pair<size_t, std::pair<size_t, IComponent *> > >::iterator	it;
+
+  it = _links.begin();
+  while (it != _links.end())
+    {
+      if ((*it).first == pin)
+  	{
+  	  return ((*it).second.second->Compute((*it).second.first));
+  	}
+      ++it;
+    }
+  return (false);
+}
+
 nts::Tristate	nts::c4030::Compute(size_t pin_num_this)
 {
+  size_t	input1;
+  size_t	input2;
 
+  if (pin_num_this == 3 || pin_num_this == 4 || pin_num_this == 10 ||
+      pin_num_this == 11)
+    {
+      if (pin_num_this == 3 || pin_num_this == 10)
+	{
+	  input1 = pin_num_this - 1;
+	  input2 = pin_num_this - 2;
+	}
+      else
+	{
+	  input1 = pin_num_this + 1;
+	  input2 = pin_num_this + 2;
+	}
+      if (alreadyLink(input1) == false)
+	throw Errors("Missing link for pin in 4030");
+      if (alreadyLink(input2) == false)
+	throw Errors("Missing link for pin in 4030");
+      if (getValueForPin(input1) ^ getValueForPin(input2))
+	return (nts::Tristate::TRUE);
+      return (nts::Tristate::FALSE);
+    }
+  throw Errors("Compute on no output pin");
+  return (nts::Tristate::UNDEFINED);
+}
+
+bool	nts::c4030::alreadyLink(size_t pin)
+{
+  std::vector<std::pair<size_t, std::pair<size_t, IComponent *> > >::iterator	it;
+
+  it = _links.begin();
+  while (it != _links.end())
+    {
+      if ((*it).first == pin)
+	return (true);
+      ++it;
+    }
+  return (false);
 }
 
 void	nts::c4030::SetLink(size_t pin_num_this, nts::IComponent &component,
 			    size_t pin_num_target)
 {
-
+  if (pin_num_this >= 1 && pin_num_this <= 13 && pin_num_this != 7)
+    {
+      if (alreadyLink(pin_num_this))
+	throw Errors("Pin already link for 4030");
+      _links.push_back(std::make_pair(pin_num_this, std::make_pair(pin_num_target, &component)));
+    }
+  else
+    throw Errors("Wrong pin for 4030");
 }
 
 void	nts::c4030::Dump() const
--- Components/c4030.hpp
+++ Components/c4030.hpp
@@ -5,13 +5,16 @@
 // Login   <victorien.fischer@epitech.eu>
 // 
 // Started on  Tue Feb 14 16:17:27 2017 Victorien Fischer
-// Last update Tue Feb 14 23:26:09 2017 Victorien Fischer
+// Last update Tue Feb 28 14:29:32 2017 Arnaud WURMEL
 //
 
 #ifndef __C4030_HPP__
 # define __C4030_HPP__
 
 #include <string>
+#include <vector>
+#include <functional>
+#include <utility>
 #include "IComponent.hpp"
 
 namespace nts
@@ -28,6 +31,13 @@ namespace nts
 					nts::IComponent &component,
 					size_t pin_num_target);
     virtual void		Dump(void) const;
+
+  public:
+    bool			alreadyLink(size_t);
+    bool			getValueForPin(size_t);
+    
+  private:
+    std::vector<std::pair<size_t, std::pair<size_t, IComponent *> > >	_links;
   };
 }
 
