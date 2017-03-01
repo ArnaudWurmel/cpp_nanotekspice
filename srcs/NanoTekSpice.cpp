2d0fe24190f3e82e853aa9bb4e563373bd06dac4
Arnaud WURMEL <arnaud.wurmel@epitech.eu>
2017-03-01 14:40:32 +0100

    Add display

--- srcs/NanoTekSpice.cpp
+++ srcs/NanoTekSpice.cpp
@@ -5,7 +5,7 @@
 // Login   <wurmel_a@epitech.net>
 // 
 // Started on  Fri Feb  3 18:36:24 2017 Arnaud WURMEL
-// Last update Tue Feb 28 17:44:38 2017 Arnaud WURMEL
+// Last update Wed Mar  1 09:20:51 2017 Arnaud WURMEL
 //
 
 #include <map>
@@ -111,6 +111,7 @@ void		nts::NanoTekSpice::start(int ac, char **av)
       ++it;
     }
   this->simulate();
+  this->display();
   while (_continue)
     {
       std::cout << PROMPT;
