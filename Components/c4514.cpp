6b131c8531eb8857aa81e30ebe7b5b61535a7503
Victorien Fischer <victorien.fischer@epitech.eu>
2017-03-01 18:28:21 +0100

    coded c4514

--- Components/c4514.cpp
+++ Components/c4514.cpp
@@ -5,25 +5,107 @@
 // Login   <victorien.fischer@epitech.eu>
 // 
 // Started on  Tue Feb 14 16:40:02 2017 Victorien Fischer
-// Last update Wed Feb 15 20:56:15 2017 Arnaud WURMEL
+// Last update Wed Mar  1 18:27:18 2017 Victorien Fischer
 //
 
+#include <string>
+#include "Errors.hpp"
 #include "c4514.hpp"
 
-nts::c4514::c4514(const std::string &value)
+const static size_t	_output[17][5] =
+  {
+    // A, B, C, D, SX
+    { 0, 0, 0, 0, 11 },
+    { 1, 0, 0, 0, 10 },
+    { 0, 1, 0, 0, 9 },
+    { 1, 1, 0, 0, 8 },
+    { 0, 0, 1, 0, 7 },
+    { 1, 0, 1, 0, 6 },
+    { 0, 1, 1, 0, 5 },
+    { 1, 1, 1, 0, 4 },
+    { 0, 0, 0, 1, 18 },
+    { 1, 0, 0, 1, 17 },
+    { 0, 1, 0, 1, 20 },
+    { 1, 1, 0, 1, 19 },
+    { 0, 0, 1, 1, 16 },
+    { 1, 0, 1, 1, 15 },
+    { 1, 1, 1, 0, 14 },
+    { 1, 1, 1, 1, 13 },
+    0
+  };
+
+nts::c4514::c4514(const std::string &value) : _links(0)
 {
   (void)value;
 }
 
-nts::Tristate	nts::c4514::Compute(size_t pin_num_this)
+bool		nts::c4514::getValueForPin(size_t pin)
 {
+  std::vector<std::pair<size_t, std::pair<size_t, IComponent *> > >::iterator	it;
 
+  it = _links.begin();
+  while (it != _links.end())
+    {
+      if ((*it).first == pin)
+	return ((*it).second.second->Compute((*it).second.first));
+      ++it;
+    }
+  return (false);
+}
+
+nts::Tristate		nts::c4514::getOutputForPin(size_t pin)
+{
+  int			i;
+
+  i = 0;
+  while (_output[i] && _output[i][4] != pin)
+    i++;
+  if (_output[i])
+    if (_output[i][0] == getValueForPin(2) && _output[i][1] == getValueForPin(3) &&
+	_output[i][2] == getValueForPin(21) && _output[i][3] == getValueForPin(22))
+      return (nts::Tristate::TRUE);
+  return (nts::Tristate::FALSE);
+}
+
+nts::Tristate	nts::c4514::Compute(size_t pin)
+{
+  if ((pin >= 4 && pin <= 11) || (pin >= 13 && pin <= 20))
+    {
+      if (getValueForPin(1))
+	return (nts::Tristate::FALSE);
+      else
+	{
+	  if (!alreadyLink(2) || !alreadyLink(3) || !alreadyLink(21) || !alreadyLink(22))
+	    throw Errors("Missing link for pin in 4514");
+	  return (getOutputForPin(pin));
+	}
+    }
+  throw Errors("Compute on no output pin");
+  return (nts::Tristate::UNDEFINED);
+}
+
+bool	nts::c4514::alreadyLink(size_t pin)
+{
+  std::vector<std::pair<size_t, std::pair<size_t, IComponent *> > >::iterator	it;
+
+  it = _links.begin();
+  while (it != _links.end())
+    {
+      if ((*it).first == pin)
+	return (true);
+      ++it;
+    }
+  return (false);
 }
 
 void	nts::c4514::SetLink(size_t pin_num_this, nts::IComponent &component,
 			    size_t pin_num_target)
 {
-
+  if (pin_num_this == 12 || pin_num_this == 24)
+    throw Errors("Wrong pin for 4514");
+  if (alreadyLink(pin_num_this))
+    throw Errors("Pin already link for 4514");
+  _links.push_back(std::make_pair(pin_num_this, std::make_pair(pin_num_target, &component)));
 }
 
 void	nts::c4514::Dump() const
--- Components/c4514.hpp
+++ Components/c4514.hpp
@@ -5,13 +5,16 @@
 // Login   <victorien.fischer@epitech.eu>
 // 
 // Started on  Tue Feb 14 16:17:27 2017 Victorien Fischer
-// Last update Tue Feb 14 23:26:18 2017 Victorien Fischer
+// Last update Wed Mar  1 18:26:20 2017 Victorien Fischer
 //
 
 #ifndef __C4514_HPP__
 # define __C4514_HPP__
 
 #include <string>
+#include <vector>
+#include <utility>
+#include <functional>
 #include "IComponent.hpp"
 
 namespace nts
@@ -28,6 +31,14 @@ namespace nts
 					nts::IComponent &component,
 					size_t pin_num_target);
     virtual void		Dump(void) const;
+
+  public:
+    bool			alreadyLink(size_t);
+    bool			getValueForPin(size_t);
+    nts::Tristate		getOutputForPin(size_t);
+    
+  private:
+    std::vector<std::pair<size_t, std::pair<size_t, IComponent *> > >	_links;
   };
 }
 
