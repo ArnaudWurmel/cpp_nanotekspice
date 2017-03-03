ce3a15eec48db4afac7dcc888175cb6ef461062d
Victorien Fischer <victorien.fischer@epitech.eu>
2017-03-03 14:39:36 +0100

    hot fix 2

--- Components/c4013.cpp
+++ Components/c4013.cpp
@@ -5,7 +5,7 @@
 // Login   <victorien.fischer@epitech.eu>
 // 
 // Started on  Tue Feb 14 16:40:02 2017 Victorien Fischer
-// Last update Fri Mar  3 14:36:43 2017 Victorien Fischer
+// Last update Fri Mar  3 14:39:13 2017 Victorien Fischer
 //
 
 #include "c4013.hpp"
@@ -92,6 +92,8 @@ nts::Tristate	nts::c4013::ComputeOutput(size_t pin_num_this)
     _previous[num] = getOutputValue(vset, vreset, vdata);
   if ((pin_num_this == 2 || pin_num_this == 12) && !(vset && vreset))
     {
+      if (_previous[num] == nts::Tristate::UNDEFINED)
+	return (_previous[num]);
       if (_previous[num] == nts::Tristate::TRUE)
 	return (nts::Tristate::FALSE);
       return (nts::Tristate::TRUE);
