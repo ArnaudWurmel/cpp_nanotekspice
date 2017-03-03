aa2778ddb4d75cd6d654dcc2f1f785c7411295ef
Victorien Fischer <victorien.fischer@epitech.eu>
2017-03-03 10:58:53 +0100

    init c4017

--- Components/c4017.cpp
+++ Components/c4017.cpp
@@ -5,16 +5,22 @@
 // Login   <victorien.fischer@epitech.eu>
 // 
 // Started on  Tue Feb 14 16:40:02 2017 Victorien Fischer
-// Last update Fri Mar  3 01:28:31 2017 Victorien Fischer
+// Last update Fri Mar  3 10:35:38 2017 Victorien Fischer
 //
 
 #include "c4017.hpp"
+#include "Errors.hpp"
 
 /*
 ** Constructor
 */
 nts::c4017::c4017(const std::string &value) : Component(value)
 {
+  size_t	i;
+
+  i = 0;
+  while (++i < 13)
+    addComputeFunction(i);
 }
 
 /*
@@ -40,9 +46,14 @@ nts::Tristate	nts::c4017::ComputeOutput(size_t pin_num_this)
 void	nts::c4017::SetLink(size_t pin_num_this, nts::IComponent &component,
 			    size_t pin_num_target)
 {
-  (void)pin_num_this;
-  (void)component;
-  (void)pin_num_target;
+  if (pin_num_this >= 13 && pin_num_this <= 15)
+    {
+      if (alreadyLink(pin_num_this))
+	throw Errors("Pin already linked for 4017");
+      _links.push_back(std::make_pair(pin_num_this, std::make_pair(pin_num_target, &component)));
+    }
+  else
+    throw Errors("Wrong pin for 4017");
 }
 
 /*
